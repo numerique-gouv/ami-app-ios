5cc1238e3119a740a2073a311e80dd67073889de
Aline Bonnet <aline.bonnet@otterways.com>
2025-10-19 15:22:39 +0200

    Initial Commit

---
+++ AMI/AMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMIApp.swift
+//  AMI
+//
+//  Created by Aline Bonnet on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AMITests/AMITests.swift
@@ -0,0 +1,17 @@
+//
+//  AMITests.swift
+//  AMITests
+//
+//  Created by Aline Bonnet on 19/10/2025.
+//
+
+import Testing
+@testable import AMI
+
+struct AMITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
