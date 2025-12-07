f6ef300adc459f5e177f05a2190a177fd4cfb682
Aline Bonnet <aline.bonnet@otterways.com>
2025-12-07 17:29:29 +0100

    add api for reviewApps

---
+++ AMI/Home/ReviewAppView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewAppView.swift
+//  AMI
+//
+//  Created by Aline Bonnet on 05/12/2025.
+//
+
+import SwiftUI
+
+struct ReviewAppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewAppView()
+}
---
+++ AMI/Models/ReviewApp.swift
@@ -0,0 +1,15 @@
+//
+//  ReviewApp.swift
+//  AMI
+//
+//  Created by Aline Bonnet on 07/12/2025.
+//
+
+import Foundation
+
+struct ReviewApp: Decodable {
+    let url: String
+    let title: String
+    let number: Int
+    let description: String?
+}
