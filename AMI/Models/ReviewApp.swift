d08f0a3af4ea39df600be5c150329775196433ca
Aline Bonnet <aline.bonnet@otterways.com>
2025-12-08 17:39:41 +0100

    add reviewApp view

--- AMI/Models/ReviewApp.swift
+++ AMI/Models/ReviewApp.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 
-struct ReviewApp: Decodable {
+struct ReviewApp: Decodable, Identifiable {
+    var id: String = UUID().uuidString
     let url: String
     let title: String
     let number: Int
     let description: String?
+    
+    enum CodingKeys: String, CodingKey {
+            case url
+            case title
+            case number
+            case description
+        }
 }
