333b6d0af50a4866d0e8f67c080284dd840e3eb8
Mathieu Agopian <mathieu@agopian.info>
2025-12-23 10:31:04 +0100

    Add a new "local dev" config

--- AMI/Components/WebView.swift
+++ AMI/Components/WebView.swift
@@ -18,11 +18,33 @@ struct WebView: UIViewRepresentable {
     
     func makeUIView(context: Context) -> some UIView {
         let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
         webView.load(URLRequest(url: url))
         return webView
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
         
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        // Handle SSL certificate challenges to accept self-signed certificates (DEBUG only)
+        func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+            #if DEBUG
+            // In debug builds, accept self-signed certificates
+            if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
+               let serverTrust = challenge.protectionSpace.serverTrust {
+                let credential = URLCredential(trust: serverTrust)
+                completionHandler(.useCredential, credential)
+                return
+            }
+            #endif
+
+            // In release builds, use default handling (reject invalid certificates)
+            completionHandler(.performDefaultHandling, nil)
+        }
     }
 }
