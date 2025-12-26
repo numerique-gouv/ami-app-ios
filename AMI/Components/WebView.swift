86c53372d0c612d4b79057a72a124baa4f6f7fc3
Mathieu Agopian <mathieu@agopian.info>
2025-12-26 17:14:14 +0100

    Display WebView console logs in xcode for debugging purposes

--- AMI/Components/WebView.swift
+++ AMI/Components/WebView.swift
@@ -17,12 +17,81 @@ struct WebView: UIViewRepresentable {
     }
     
     func makeUIView(context: Context) -> some UIView {
-        let webView = WKWebView()
+        let configuration = WKWebViewConfiguration()
+        let contentController = WKUserContentController()
+
+        // JavaScript to capture console logs
+        let consoleLogScript = """
+        (function() {
+            function sendLog(level, args) {
+                var message = Array.prototype.slice.call(args).map(function(arg) {
+                    if (typeof arg === 'object') {
+                        try {
+                            return JSON.stringify(arg);
+                        } catch (e) {
+                            return String(arg);
+                        }
+                    }
+                    return String(arg);
+                }).join(' ');
+
+                window.webkit.messageHandlers.consoleLog.postMessage({
+                    level: level,
+                    message: message
+                });
+            }
+
+            var originalLog = console.log;
+            var originalWarn = console.warn;
+            var originalError = console.error;
+            var originalInfo = console.info;
+            var originalDebug = console.debug;
+
+            console.log = function() {
+                sendLog('log', arguments);
+                originalLog.apply(console, arguments);
+            };
+
+            console.warn = function() {
+                sendLog('warn', arguments);
+                originalWarn.apply(console, arguments);
+            };
+
+            console.error = function() {
+                sendLog('error', arguments);
+                originalError.apply(console, arguments);
+            };
+
+            console.info = function() {
+                sendLog('info', arguments);
+                originalInfo.apply(console, arguments);
+            };
+
+            console.debug = function() {
+                sendLog('debug', arguments);
+                originalDebug.apply(console, arguments);
+            };
+        })();
+        """
+
+        let userScript = WKUserScript(source: consoleLogScript, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+        contentController.addUserScript(userScript)
+        contentController.add(context.coordinator, name: "consoleLog")
+
+        configuration.userContentController = contentController
+
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        context.coordinator.webView = webView
         webView.load(URLRequest(url: url))
         return webView
+
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
         
+    }
+
+    func makeCoordinator() -> WebViewCoordinator {
+        WebViewCoordinator()
     }
 }
