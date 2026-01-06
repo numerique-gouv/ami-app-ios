980ffef136645c3800d535682a1cfdf92b77c156
Mathieu Agopian <mathieu@agopian.info>
2026-01-06 19:14:10 +0100

    First go back in the WebView's history if possible, before going back through the native views
    This also takes the swipe from left gesture familiar to iPhone users

--- AMI/Home/HomeView.swift
+++ AMI/Home/HomeView.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct HomeView: View {
+    @Environment(\.dismiss) var dismiss
     @State var isExternalProcess = false
     @State var currentURL = Config.shared.BASE_URL
     @State var lastURL = Config.shared.BASE_URL // Not used for now
-    
+    @State var webViewRef: WKWebView? = nil
+
     var body: some View {
         VStack {
             if(isExternalProcess){
@@ -20,7 +23,34 @@ struct HomeView: View {
                     currentURL = Config.shared.BASE_URL
                 }
             }
-            WebView(isExternalProcess: $isExternalProcess, currentURL: $currentURL, lastURL: $lastURL)
+            WebView(isExternalProcess: $isExternalProcess, currentURL: $currentURL, lastURL: $lastURL, webViewRef: $webViewRef)
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: handleBackAction) {
+                    HStack {
+                        Image(systemName: "chevron.left")
+                        Text("Retour")
+                    }
+                }
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onEnded { gesture in
+                    if gesture.translation.width > 50 {
+                        handleBackAction()
+                    }
+                }
+        )
+    }
+
+    private func handleBackAction() {
+        if let webView = webViewRef, webView.canGoBack {
+            webView.goBack()
+        } else {
+            dismiss()
         }
     }
 }
