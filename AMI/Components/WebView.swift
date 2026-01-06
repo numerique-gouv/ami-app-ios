9bb7a4306622d0aeda4c05b2d71fbd4f4d2f12e0
Mathieu Agopian <mathieu@agopian.info>
2026-01-06 14:53:46 +0100

    Simplify the back button: use a ref to the webview

--- AMI/Components/WebView.swift
+++ AMI/Components/WebView.swift
@@ -10,20 +10,23 @@ import Foundation
 import WebKit
 
 struct WebView: UIViewRepresentable {
+    let initialURL: String
     @Binding var isExternalProcess: Bool
-    @Binding var currentURL: String
-    @Binding var lastURL: String
+    @Binding var webViewRef: WKWebView?
     @State var webView: WKWebView = WKWebView()
-    
+
     func makeUIView(context: Context) -> some UIView {
         webView.navigationDelegate = context.coordinator
-        context.coordinator.observeURL(of: self)
-        webView.load(URLRequest(url: URL(string: currentURL)!))
+        webView.load(URLRequest(url: URL(string: initialURL)!))
+
+        DispatchQueue.main.async { // Do not modify the state during view update.
+            webViewRef = webView
+        }
+
         return webView
     }
-    
+
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        webView.load(URLRequest(url: URL(string: currentURL)!))
     }
     
     func makeCoordinator() -> WebViewCoordinator {
