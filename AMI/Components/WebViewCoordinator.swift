415236d5f76d2d13b4f618f9ae63534dd99e397c
Mathieu Agopian <mathieu@agopian.info>
2026-01-12 15:10:57 +0100

    Display a progress bar above the WebView showing the loading progress when clicking on a link

--- AMI/Components/WebViewCoordinator.swift
+++ AMI/Components/WebViewCoordinator.swift
@@ -7,15 +7,27 @@
 
 import Foundation
 @preconcurrency import WebKit
+import SwiftUI
 
 class WebViewCoordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
     var parent: WebView
-    
-    init(_ parent: WebView) {
+    private var progressObservation: NSKeyValueObservation?
+    private var isLoadingBinding: Binding<Bool>
+    private var loadingProgressBinding: Binding<Double>
+
+    init(_ parent: WebView, isLoading: Binding<Bool>, loadingProgress: Binding<Double>) {
         self.parent = parent
+        self.isLoadingBinding = isLoading
+        self.loadingProgressBinding = loadingProgress
     }
     
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        // Show loader immediately on link click (before page starts loading)
+        DispatchQueue.main.async {
+            self.isLoadingBinding.wrappedValue = true
+            self.loadingProgressBinding.wrappedValue = 0.0
+        }
+
         guard let urlString = navigationAction.request.url?.absoluteString else { return }
         parent.isExternalProcess = !urlString.contains(Config.shared.BASE_URL)
 
@@ -44,6 +56,21 @@ class WebViewCoordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler
             case "consoleLog": return ConsoleLog.printLog(message)
         default:
             return
+        }
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        DispatchQueue.main.async {
+            self.isLoadingBinding.wrappedValue = false
+        }
+    }
+
+    func observeProgress(of wkWebView: WKWebView) {
+        progressObservation = wkWebView.observe(\.estimatedProgress, options: [.new]) { [weak self] webView, _ in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.loadingProgressBinding.wrappedValue = webView.estimatedProgress
+            }
         }
     }
 }
--- AMI/Home/HomeView.swift
+++ AMI/Home/HomeView.swift
@@ -11,15 +11,22 @@ import WebKit
 struct HomeView: View {
     @State var isExternalProcess = false
     @State var webViewRef: WKWebView?
+    @State var isLoading = false
+    @State var loadingProgress: Double = 0.0
 
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             if(isExternalProcess){
                 BackBar() {
                     webViewRef?.load(URLRequest(url: URL(string: Config.shared.BASE_URL)!))
                 }
             }
-            WebView(initialURL: Config.shared.BASE_URL, isExternalProcess: $isExternalProcess, webViewRef: $webViewRef)
+            if isLoading {
+                ProgressView(value: loadingProgress)
+                    .progressViewStyle(.linear)
+                    .tint(.blue)
+            }
+            WebView(initialURL: Config.shared.BASE_URL, isExternalProcess: $isExternalProcess, webViewRef: $webViewRef, isLoading: $isLoading, loadingProgress: $loadingProgress)
         }
     }
 }
