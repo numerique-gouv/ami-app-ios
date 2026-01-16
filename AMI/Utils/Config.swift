941cfdf7a1b09bb49b48dead857633dcb1d3ce55
Nicolas Buquet <github@buquet-net.com>
2026-01-16 07:45:12 +0100

    Config values code optimization

--- AMI/Utils/Config.swift
+++ AMI/Utils/Config.swift
@@ -10,17 +10,17 @@ class Config {
     static let shared: Config = Config()
     
     private init() {
-        BASE_URL = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String ?? ""
+        BASE_URL = Self.getValue(for: "BASE_URL")
     }
     
     var BASE_URL: String
-    var CURRENT_ENV: String { return self.getValue(for: "CURRENT_ENV")}
     
-    private func getValue(for key : String) -> String {
-        guard let value = Bundle.main.object(forInfoDictionaryKey: key) as? String else {
-            return "DEFAUT_VALUE_FOR_\(key)"
-        }
-        return value
+    var CURRENT_ENV: String {
+        return Self.getValue(for: "CURRENT_ENV", defaultValue: "DEFAUT_VALUE_FOR_CURRENT_ENV")
+    }
+    
+    static func getValue(for key : String, defaultValue: String = "") -> String {
+        Bundle.main.object(forInfoDictionaryKey: key) as? String ?? defaultValue
     }
 }
 
