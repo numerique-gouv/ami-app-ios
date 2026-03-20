1457764556001e231376fb80d615434c326673ad
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-03-20 11:32:25 +0100

    Optimize AMIWebView structure to avoir reloading (and multiple back buttons in toolbar)

--- AMI/Sources/Components/WebView/AMIWebView.swift
+++ AMI/Sources/Components/WebView/AMIWebView.swift
@@ -23,25 +23,29 @@ struct AMIWebView: View {
         SwiftUIWebView(viewModel: viewModel)
     }
 
-    @ViewBuilder
-    func backButton(action: (() -> Void)?) -> some View {
-        HStack {
-            Button {
-                action?()
-            } label: {
-                Label(AMIL10n.commonBack, systemImage: "arrowtriangle.left.fill")
-                    .labelStyle(.titleAndIcon) // needed for title to be displayed when located in toolbar.
-                    .fixedSize() // needed for title to be fully displayed.
-            }
-            Spacer()
-        }
-    }
+//    @ViewBuilder
+//    func backButton(action: (() -> Void)?) -> some View {
+//        HStack {
+//            Button {
+//                action?()
+//            } label: {
+//                Label(AMIL10n.commonBack, systemImage: "arrowtriangle.left.fill")
+//                    .labelStyle(.titleAndIcon) // needed for title to be displayed when located in toolbar.
+//                    .fixedSize() // needed for title to be fully displayed.
+//            }
+//            Spacer()
+//        }
+//    }
 
     var body: some View {
-        if viewModel.isLoading {
-            loadingBar
+        ZStack(alignment: .top) {
+            SwiftUIWebView(viewModel: viewModel)
+            if viewModel.isLoading {
+                loadingBar
+                    .allowsHitTesting(false)
+            }
         }
-        SwiftUIWebView(viewModel: viewModel)
+        .navigationBarBackButtonHidden()
     }
 }
 
