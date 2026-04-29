6de825ec25ca55c1fbe1308586709a7da96dce8d
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-04-29 10:05:48 +0200

    Define root URL with final slash (more standard and more compatible with backTo RootUrl action)

--- AMI/Sources/Utils/Config.swift
+++ AMI/Sources/Utils/Config.swift
@@ -10,7 +10,7 @@ final class Config {
     static let shared = Config()
 
     #if IS_AMI_STAGING
-        let BASE_URL = URL(string: "https://ami-back-staging.osc-fr1.scalingo.io")!
+        let BASE_URL = URL(string: "https://ami-back-staging.osc-fr1.scalingo.io/")!
         let OIDC_HOSTS = [
             "fcp-low.sbx.dev-franceconnect.fr",
             "fip1-low.sbx.fcp.fournisseur-d-identite.fr", // Démo eIDAS faible
@@ -19,7 +19,7 @@ final class Config {
             "ami-fc-proxy-dev.osc-fr1.scalingo.io", // Utilisé par le bouton "Revenir sur AMI" de dev-franceconnect.fr
         ]
     #elseif IS_AMI_PRODUCTION
-        let BASE_URL = URL(string: "https://ami-back-prod.osc-secnum-fr1.scalingo.io")!
+        let BASE_URL = URL(string: "https://ami-back-prod.osc-secnum-fr1.scalingo.io/")!
         let OIDC_HOSTS = [
             "oidc.franceconnect.gouv.fr",
             "cfspart-idp.impots.gouv.fr", // FI impots.gouv.fr
