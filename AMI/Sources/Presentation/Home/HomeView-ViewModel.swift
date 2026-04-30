af921b992c5d490670d00e9569165fa8a70fba44
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-04-30 18:51:32 +0200

    Use SpecialWebPageUrl enum to detect special web pages in HomeView-ViewModel

--- AMI/Sources/Presentation/Home/HomeView-ViewModel.swift
+++ AMI/Sources/Presentation/Home/HomeView-ViewModel.swift
@@ -41,13 +41,13 @@ extension HomeView {
 
         @Sendable
         private func handleUrlChange(webViewViewModel: SwiftUIWebView.ViewModel, url: URL?) {
-            showSettings = url?.absoluteString.hasSuffix("/#/preferences") ?? false
-            isOnContactPage = url?.absoluteString.hasSuffix("/#/contact") ?? false
+            let shoudlShowNotificationsSettings = SpecialWebPageUrl.notificationsSettings.match(url)
+            isOnContactPage = SpecialWebPageUrl.contact.match(url)
 
             print("[HomeView-ViewModel]: URL Change Action \(url?.debugDescription ?? "<nil>")\n\tsettings: \(showSettings) - contact: \(isOnContactPage)")
 
             Task { @MainActor in
-                if self.showSettings,
+                if shoudlShowNotificationsSettings,
                    self.webViewViewModel.webView?.canGoBack ?? false {
                     // As new page should not be handled by webview, reset webView last step navigation (to clean history).
                     self.webViewViewModel.webView?.goBack()
