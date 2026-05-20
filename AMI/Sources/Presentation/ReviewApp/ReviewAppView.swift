4230f6d3a8c70757a7f65fb3185c5e7992c31b36
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-05-20 18:40:02 +0200

    Remove Empty webService

--- AMI/Sources/Presentation/ReviewApp/ReviewAppView.swift
+++ AMI/Sources/Presentation/ReviewApp/ReviewAppView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ReviewAppView: View {
-    @EnvironmentObject var webService: WebService
+//    @EnvironmentObject var webService: WebService
     @State private var reviewApps: [ReviewApp] = []
     @Bindable var viewModel: ReviewAppView.ViewModel
 
@@ -40,5 +40,5 @@ struct ReviewAppView: View {
 #Preview {
     let viewModel = ReviewAppView.ViewModel(rootUrl: Config.shared.BASE_URL, notificationManager: NotificationManager())
     ReviewAppView(viewModel: viewModel)
-        .environmentObject(WebService())
+//        .environmentObject(WebService())
 }
