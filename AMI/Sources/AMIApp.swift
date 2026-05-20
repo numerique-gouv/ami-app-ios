2ab9b301b8dbbc8a4bc265214e5034929c0c54d2
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-05-20 18:39:15 +0200

    Inject RootUrl and fetchReviewApps method in ReviewApp ViewModel

--- AMI/Sources/AMIApp.swift
+++ AMI/Sources/AMIApp.swift
@@ -34,7 +34,7 @@ struct AMIApp: App {
                         HomeView(viewModel: appState.notificationTriggeredHomeViewModel)
                             .id(notificationActivatedHomeViewModelId)
                     } else {
-                        ReviewAppView(viewModel: ReviewAppView.ViewModel(notificationManager: Self.notificationManager))
+                        ReviewAppView(viewModel: ReviewAppView.ViewModel(rootUrl: Config.shared.BASE_URL, notificationManager: AMIAppState.notificationManager))
                             .environmentObject(WebService())
                     }
                 #elseif IS_AMI_PRODUCTION
--- AMI/Sources/Presentation/ReviewApp/ReviewAppView.swift
+++ AMI/Sources/Presentation/ReviewApp/ReviewAppView.swift
@@ -20,7 +20,7 @@ struct ReviewAppView: View {
         Text("Choix de la review")
             .font(.title)
         ScrollView {
-            ForEach(reviewApps) { reviewApp in
+            ForEach(viewModel.reviewApps) { reviewApp in
                 if let reviewAppUrl = URL(string: reviewApp.url) {
                     NavigationLink(value: reviewAppUrl) {
                         Tile(title: reviewApp.title,
@@ -34,18 +34,11 @@ struct ReviewAppView: View {
                 }
             }
         }
-        .task {
-            Task {
-                try await webService.getReviewApps()
-                reviewApps.removeAll()
-                reviewApps.append(contentsOf: webService.reviewApps)
-            }
-        }
     }
 }
 
 #Preview {
-    let viewModel = ReviewAppView.ViewModel(notificationManager: NotificationManager())
+    let viewModel = ReviewAppView.ViewModel(rootUrl: Config.shared.BASE_URL, notificationManager: NotificationManager())
     ReviewAppView(viewModel: viewModel)
         .environmentObject(WebService())
 }
