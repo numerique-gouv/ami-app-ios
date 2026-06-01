1cb2a6cdb166b353d7db3c12a5015060b3d98cf1
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-06-01 16:55:55 +0200

    Fix typo

--- AMI/Sources/Components/WebView/WebView-ViewModel.swift
+++ AMI/Sources/Components/WebView/WebView-ViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 import WebKit
 
 extension SwiftUIWebView {
-    // Configguration that can be shared by all SwiftUIWebView to access the same cookie store.
+    // Configuration that can be shared by all SwiftUIWebView to access the same cookie store.
     static let sharedConfiguration = WKWebViewConfiguration()
 
     @Observable
