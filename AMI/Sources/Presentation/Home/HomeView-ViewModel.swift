86a169e7d706eba3719837f7d0fbf428a1d2ef66
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-06-01 16:55:16 +0200

    Call deleteSessionLocalData on user logout

--- AMI/Sources/Presentation/Home/HomeView-ViewModel.swift
+++ AMI/Sources/Presentation/Home/HomeView-ViewModel.swift
@@ -141,10 +141,11 @@ extension HomeView {
             // Reset Notification status check when on logout to recheck it on next login.
             checkNotificationStatusDone = false
 
-            // TODO: we should reset web session here to destroy any user data.
-            // Currently, on next login, FC find the previous token and reconnect automatically with previous profile.
-
-            webViewViewModel.goBackToRootUrl()
+            Task { @MainActor in
+                // Remove all session data to avoid reusing them on next connection.
+                await webViewViewModel.deleteSessionLocalData()
+                webViewViewModel.goBackToRootUrl()
+            }
         }
 
         func partnerViewModel(for url: URL) -> PartnerView.ViewModel {
