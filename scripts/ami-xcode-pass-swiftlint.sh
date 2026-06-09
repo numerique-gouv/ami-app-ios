bfa2ada4e8f815e8ac84cccc4f4d26f37de749b7
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-06-09 11:22:12 +0200

    auto-fix trailing whitespaces warnings first when compiling.

--- scripts/ami-xcode-pass-swiftlint.sh
+++ scripts/ami-xcode-pass-swiftlint.sh
@@ -2,7 +2,8 @@
 
 export PATH="$PATH:/opt/homebrew/bin"
 if command -v swiftlint >/dev/null 2>&1; then
-    swiftlint
+# auto-fix trailing whitespaces warnings first.
+    swiftlint --fix --only-rule trailing_whitespace && swiftlint
 else
     echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
