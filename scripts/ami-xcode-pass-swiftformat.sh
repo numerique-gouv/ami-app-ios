0f87fa59bef02aa21431ceac17786d1c88058735
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-06-10 12:54:06 +0200

    Enhance SwiftFormat pass (autofix trailing spaces)

--- scripts/ami-xcode-pass-swiftformat.sh
+++ scripts/ami-xcode-pass-swiftformat.sh
@@ -2,7 +2,8 @@
 
 export PATH="$PATH:/opt/homebrew/bin"
 if command -v swiftformat >/dev/null 2>&1; then
-    swiftformat --lint --lenient "$PROJECT_DIR"
+# auto-fix trailing whitespaces warnings first.
+	swiftformat "${SRCROOT}" --rules trailingSpace && swiftformat --lint --lenient "$PROJECT_DIR"
 else
     echo "warning: SwiftFormat not installed, download from https://github.com/nicklockwood/SwiftFormat"
 fi
