8681d451da8f21a06e09e15483e828346cc5f4fb
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-06-10 19:38:41 +0200

    Remove `requireAuthentication` parameter from `deleteAll` method because it is useless: all items .medium/.high are deleted

---
+++ AMI/Sources/Presentation/Settings/LocalStorageTestView-ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LocalStorageTestView-ViewModel.swift
+//  AMI-xcodegen
+//
+//  Created by Nicolas Buquet on 10/06/2026.
+//  Copyright © 2026 DINUM. All rights reserved.
+//
+
+import Foundation
