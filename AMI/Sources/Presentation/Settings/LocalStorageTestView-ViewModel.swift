2d05109718c8ab9e7d24dfb4baf8c72a155aaaed
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-06-11 15:19:30 +0200

    Remove test UI files

--- AMI/Sources/Presentation/Settings/LocalStorageTestView-ViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LocalStorageTestView-ViewModel.swift
-//  AMI-xcodegen
-//
-//  Created by Nicolas Buquet on 10/06/2026.
-//  Copyright © 2026 DINUM. All rights reserved.
-//
-
-import Foundation
