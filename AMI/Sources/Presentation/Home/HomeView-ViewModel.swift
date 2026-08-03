23a9c1ace328626f6c15e9e0367db2540b8d0468
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-08-03 17:49:43 +0200

    Isolate Native infos script from User scripts because it is initialised asynchronously
    # Conflicts:
    #	AMI/Sources/Presentation/Home/HomeView-ViewModel.swift

--- AMI/Sources/Presentation/Home/HomeView-ViewModel.swift
+++ AMI/Sources/Presentation/Home/HomeView-ViewModel.swift
@@ -130,9 +130,9 @@ extension HomeView {
             // Set NotificationManager base URL to register the device to AMI backend to allow Push Notifications.
             setNotificationManagerBaseUrl(rootUrl)
 
-            // Load initial page now that viewModel is fully ready.
-            Task { @MainActor in
-                webViewViewModel.loadInitialPage()
+            Task.detached(priority: .background) { @MainActor in
+                let nativeInfosScript = await HomeNativeInfosScripts()
+                webViewViewModel.addUserScripts(userScripts: nativeInfosScript)
             }
         }
 
