1fa67d07905a028f7e5ca8fa3823d124ffa28819
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-08-17 10:04:22 +0200

    Update to Swift to read domain string and not URL for baseUrl

--- AMI/Sources/Utils/Config.swift
+++ AMI/Sources/Utils/Config.swift
@@ -9,8 +9,8 @@ import Foundation
 final class Config {
     static let shared = Config()
 
-    let BASE_URL = URL(string: Secrets.baseUrlString)!
-    let OIDC_HOSTS = Secrets.oidcHosts
+    let BASE_URL = URL(string: "https://\(Secrets.baseDomainString)/")!
+    let OIDC_HOSTS = Secrets.oidcDomainsStrigns
 
     private init() {}
 }
