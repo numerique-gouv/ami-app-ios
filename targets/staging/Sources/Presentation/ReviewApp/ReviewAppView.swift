45de8de8b4a423ceca1feee7eafbc18cddfebdb6
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-09-02 11:11:41 +0200

    (Lint) Simplify test to accept to list review app

--- targets/staging/Sources/Presentation/ReviewApp/ReviewAppView.swift
+++ targets/staging/Sources/Presentation/ReviewApp/ReviewAppView.swift
@@ -21,7 +21,7 @@ struct ReviewAppView: View {
             .font(.title)
         ScrollView {
             ForEach(viewModel.reviewApps) { reviewApp in
-                if let reviewAppUrl = URL(string: reviewApp.url) {
+                if URL(string: reviewApp.url) != nil {
                     Button {
                         viewModel.selectReviewApp(reviewApp: reviewApp)
                     } label: {
