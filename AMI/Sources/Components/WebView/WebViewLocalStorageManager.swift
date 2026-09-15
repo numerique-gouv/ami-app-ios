a037dbc1aee0f6d8f0ea1bb64473885aedec6c7b
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-09-15 16:19:36 +0200

    Implement cookie cleaning in newly created struct `WebViewLocalStorageManager`

---
+++ AMI/Sources/Components/WebView/WebViewLocalStorageManager.swift
@@ -0,0 +1,103 @@
+//
+//  WebViewLocalStorageManager.swift
+//  AMI-xcodegen
+//
+//  Created by Nicolas Buquet on 15/09/2026.
+//  Copyright © 2026 DINUM. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+struct WebViewLocalStorageManager {
+    private weak var webView: WKWebView?
+
+    init(webView: WKWebView) {
+        self.webView = webView
+    }
+
+    @MainActor // `evaluateJavaScript` must be used from main thread only.
+    func readInLocalStorage(key: String) async -> Any? {
+        guard let webView else {
+            AppLog.viewModel.notice("\(AppLog.logHeader(self)) ReadInLocalStorage not called because no webView initialzed")
+            return nil
+        }
+
+        // Prepare javaScript script.
+        let script = "localStorage.getItem('\(key)');"
+
+        do {
+            // Execute javaScript script
+            let value = try await webView.evaluateJavaScript(script)
+            AppLog.viewModel.notice("\(AppLog.logHeader(self)) ReadInLocalStorage success - `\(key)` -> `\(value.debugDescription, privacy: .private)`")
+            return value
+        } catch {
+            AppLog.viewModel.notice("\(AppLog.logHeader(self)) ReadInLocalStorage failed to read key `\(key)`: \(error)")
+            return nil
+        }
+    }
+
+    @MainActor // `evaluateJavaScript` must be used from main thread only.
+    func writeInLocalStorage(key: String, value: String) async {
+        guard let webView else {
+            AppLog.viewModel.notice("\(AppLog.logHeader(self)) WriteInLocalStorage not called because no webView initialzed")
+            return
+        }
+
+        // Prepare javaScript script.
+        let script = "localStorage.setItem('\(key)', '\(value)');"
+
+        do {
+            // Execute javaScript script
+            _ = try await webView.evaluateJavaScript(script)
+            AppLog.viewModel.notice("\(AppLog.logHeader(self)) WriteInLocalStorage success - `\(key)` = `\(value, privacy: .private)`")
+        } catch {
+            AppLog.viewModel.notice("\(AppLog.logHeader(self)) WriteInLocalStorage failed to set key `\(key)` to value `\(value, privacy: .private)`: \(error)")
+        }
+    }
+
+    /// Delete all local data and cookies associated with the current web session.
+    @MainActor
+    func deleteSessionLocalData() async {
+        // Check if website data store store exists.
+        guard let websiteDataStore = webView?.configuration.websiteDataStore else {
+            return
+        }
+
+        // List data types to clear, excluding cookies that will be treated separately (a special cookie need to be preserved).
+        var dataTypesToClear = WKWebsiteDataStore.allWebsiteDataTypes()
+        dataTypesToClear.remove(WKWebsiteDataTypeCookies)
+        let records = await websiteDataStore.dataRecords(ofTypes: dataTypesToClear)
+        for record in records {
+            await websiteDataStore.removeData(ofTypes: record.dataTypes, for: [record])
+        }
+
+        // Delete cookies except FranceConnect special cookie.
+        await deleteCookiesExceptFranceConnectTrustedDevice()
+    }
+
+    /// Delete all local cookies except a FranceConnect cookie named `trustedDevice`.
+    @MainActor
+    private func deleteCookiesExceptFranceConnectTrustedDevice() async {
+        // Check if website data store store exists.
+        guard let websiteDataStore = webView?.configuration.websiteDataStore else {
+            return
+        }
+
+        // Get cookies store.
+        let cookieStore = websiteDataStore.httpCookieStore
+
+        // Grab cookie to preserve.
+        let protectedCookie = await cookieStore.allCookies().first {
+            $0.name == "trustedDevice" && $0.domain == Secrets.franceconnectTrustedDeviceCookieDomain
+        }
+
+        // Delete all cookies.
+        await websiteDataStore.removeData(ofTypes: [WKWebsiteDataTypeCookies], modifiedSince: .distantPast)
+
+        // Restore cookie to preserve if it exists.
+        if let protectedCookie {
+            await cookieStore.setCookie(protectedCookie)
+        }
+    }
+}
